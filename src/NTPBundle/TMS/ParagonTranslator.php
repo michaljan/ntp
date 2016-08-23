c5f875f072de0755212c293ec5def76b87f63f40
Michal <Michal@Laptop>
2016-08-23 22:16:24 +0100

    TMS beginning - data move

--- src/NTPBundle/TMS/ParagonTranslator.php
+++ src/NTPBundle/TMS/ParagonTranslator.php
@@ -13,7 +13,7 @@ class ParagonTranslator{
     
     public function convertData(){
         $query = $this->em
-                ->createQuery("INSERT INTO NTPBundle:Schedule () "
+                ->createQuery("INSERT INTO NTPBundle:Schedule () routeNo,s.startTimePlanned,s.departTimePlanned,s.arrivalTimePlanned,s.finishTimePlanned,s.dutyTimePlanned,s.routeDistance,s.routeTime,s.kmsPlanned "
                         . "SELECT DISTINCT p.routeNo, p.tripNo ,p.callTripPosition ,p.depotId ,p.startTime ,p.sourceDepotDepartureTime ,p.customerId ,p.customerName ,p.postcode ,p.timeWindowStart ,p.timeWindowEnd ,p.arrivalTime ,p.departTime ,p.callDuration ,p.callType ,p.orderDetails1 ,p.measure1 ,p.measure2 ,p.measure3 ,p.measure4 ,p.measure5 ,p.prodCode ,p.productName ,p.trailerGroupName ,p.orderDetails2 ,p.orderDetails3 ,p.orderDetails4 ,p.endDepotArrivalTime ,p.dutyTime ,p.driveTime ,p.distanceKms ,p.emptyDistKms ,p.emptyTime ,p.timeUtil ,p.waitingTime ,p.noOfTrips ,p.routeDropNo ,p.callNo ,p.tripDropNo ,p.tripStart ,p.destDepotArrivalTime ,p.destDepotDepartureTime ,p.tripsEndDepot ,p.tripsSourceDepot ,p.sourceDepotArrivalTime2 ,p.sourceDepotDepartureTime2 ,p.tripsStartDepot ,p.startDepotDepartureTime ,p.trailerTypeName ,p.endTime ,p.transferId ,p.driverGroupName ,p.tractorGroupName ,p.callRefNumber ,p.travelDistNext ,p.travelDistPrev ,p.custData1 ,p.custData2 ,p.custData3 ,p.ndata1 ,p.ndata2 ,p.ndata3 ,p.ndata4 ,p.ndata5 ,p.sdata1 ,p.sdata2 ,p.sdata3 ,p.sdata4 ,p.sdata5 ,p.uploadDate ,p.uploadedBy ,p.planDate ,p.planName ,p.routeName  "
                         . "FROM NTPBundle:ParagonData p WHERE p.planDate = :date ")
                 ->setParameter('date', $date);
