65a08403528198167fd3358f5f10b19c08a17977
Michal <Michal@Laptop>
2017-05-31 22:29:37 +0100

    Cron job without mailer

--- src/NTPBundle/Controller/PDFTestController.php
+++ src/NTPBundle/Controller/PDFTestController.php
@@ -9,12 +9,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use \Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
 class PDFTestController extends Controller{
 
     /**
      * @Route("/pdftest")
      */
     public function pdfTestAction() {
+        $mailer=new \NTPBundle\Mailer\CustomMailer;
+        $mailer->weekExtractMail();
         return new response($this->renderView('NTPBundle:PDFReports:weekVolumes.html.twig', array()));
     }
 
---
+++ src/NTPBundle/Mailer/CustomMailer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace NTPBundle\Mailer;
+
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class CustomMailer {
+    
+    private $mailer;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->mailer = $mailer;
+    }
+    
+    
+    public function weekExtractMail() {
+        
+        $message = \Swift_Message::newInstance()
+                ->setSubject('Hello Email')
+                ->setFrom('no-reply@mxmelite.com')
+                ->setTo('michal.papke@wp.pl')
+                ->setBody("Test"
+        );
+        //$this->get('mail.helper')->send($message);
+        $this->mailer->send($message);
+        return $this;
+    }
+
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
