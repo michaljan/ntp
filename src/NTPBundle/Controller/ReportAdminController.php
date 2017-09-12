184ab6f84864dc94554b8d78ccdd9cdce0924a7d
Michal <Michal@Laptop>
2017-09-12 15:34:19 +0100

    Finished manual email page

--- src/NTPBundle/Controller/ReportAdminController.php
+++ src/NTPBundle/Controller/ReportAdminController.php
@@ -7,17 +7,30 @@ namespace NTPBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ReportAdminController extends Controller {
 
-    public function manualEmailTriggerAction() {
-        $weeklyArray = array('weeklyVolumeEmailAction' => 'Send last week volume report', 'weeklyTractorUsageAction' => 'Send last week tractor usage email');
-        return new response($this->renderView('NTPBundle:ReportAdmin:manualEmailTrigger.html.twig', array('weeklyArray' => $weeklyArray)));
-    }
-
-    public function manualEmailAction(Request $request) {
+    public function manualEmailTriggerAction(Request $request) {
+        $weeklyArray = array('weeklyVolumeEmail' => 'Send last week volume report', 'weeklyTractorUsage' => 'Send last week tractor usage email');
         $isAjax = $request->isXmlHttpRequest();
-    }
 
+        if ($isAjax == True) {
+            $report = $request->get('data1');
+            if($report=='weeklyVolumeEmail'){
+                $data = $this->get('app.pdf_generate')->pdfVolumePrepare();
+            }
+            if($report=='weeklyTractorUsage'){
+                $data = $this->get('app.pdf_generate')->pdfTractorUsagePrepare();
+            }
+            if (!is_null($data)) {
+                $this->get('app.custom_mailer')->weekExtractMail($data);
+            }
+            $response = array('code' => 100, 'success' => true);
+            return new JsonResponse($response);
+        }
+
+        return new Response($this->renderView('NTPBundle:ReportAdmin:manualEmailTrigger.html.twig', array('weeklyArray' => $weeklyArray)));
+    }
 
 }
