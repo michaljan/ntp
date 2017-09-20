7b2062341a23286810b8cfaf3c73dfbf8e06e0dd
Michal <Michal@Laptop>
2017-09-20 14:04:46 +0100

    Started wickes reporting website

--- src/NTPBundle/Reports/WickesParagonReports.php
+++ src/NTPBundle/Reports/WickesParagonReports.php
@@ -16,10 +16,30 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 class WickesParagonReports extends ContainerAware{
 
     private $em;
+    private $date;
 
     public function __construct(EntityManager $em) {
         $this->em = $em;
     }
+    
+    
+    public function createDashboard($date){
+        $this->date=$date;
+        $networkRuns=$this->networkRuns();
+        
+        
+
+        \Doctrine\Common\Util\Debug::dump($networkRuns);
+        die;
+        return $result;
+    }
+    
+    private function networkRuns(){
+        $this->em->createQuery(
+                );
+        return $networkRuns;
+    }
+    
     
     
 
